e772ffc52fefd667bb805eb0aac89d3dc502ba72
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-07-26 22:49:20 -0400

    created toggle and axios request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Switch from "./Switch";
+import AuthService from './AuthService';
 
 function App() {
+  const [value, setValue] = useState(false);
+  const handleSubmit = async (e) => {
+		setValue(!value);
+    AuthService.postTimeINOUT(value);
+    console.log(value ? "Posted In time" : "Posted Out time")
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>inTIME</h1>
+      <Switch
+        isOn={value}
+        onColor="#EF476F"
+        handleToggle={handleSubmit}
+      />
+      <h2>OUT &nbsp;&nbsp;IN</h2>
+    
+
+
+      <form action="/createNewUser" method="POST"> 
+      <button>Create New User</button> 
+      </form> 
+      <form action="/gettotalhours" method="GET"> 
+      <button>Get total hours</button> 
+      </form> 
+      <form action="/generateReport" method="GET"> 
+      <button>Generate Report</button> 
+      </form> 
+
     </div>
   );
 }
---
+++ src/AuthService.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+const API_URL = `http://localhost:8080`;
+
+//axios call to server
+
+//signup post to server
+
+const signup = (firstName, lastName, email, password) => {
+	return axios
+		.post(API_URL + '/newuser', {
+			firstName,
+			lastName,
+			email,
+			password,
+		})
+		.catch((e) => {
+			throw e.response.data.Error;
+		});
+};
+
+//login post to server
+const login = (email, password) => {
+	return axios
+		.post(API_URL + '/auth', {
+			email,
+			password,
+		})
+		.then((response) => {
+			if (response.data.accessToken) {
+				localStorage.setItem('user', JSON.stringify(response.data));
+			}
+			return response.data;
+		});
+};
+
+// logout removes token from localstorage
+const logout = () => {
+	localStorage.removeItem('user');
+};
+
+// gets current user data from local storage
+const getCurrentUser = () => {
+	return JSON.parse(localStorage.getItem('user'));
+};
+
+const postTimeINOUT = (Value) => {
+    return axios   
+		.post(API_URL + '/postintime', { body: Value })
+		.then((response) => {
+			return response.data;
+		});
+}
+
+const AuthService = {
+	signup,
+	login,
+	logout,
+	getCurrentUser,
+    postTimeINOUT,
+};
+
+export default AuthService;
